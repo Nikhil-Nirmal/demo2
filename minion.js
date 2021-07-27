a4a77547356a10d289a3671006564ae661a7bc2c
Nikhil-Nirmal <87899262+Nikhil-Nirmal@users.noreply.github.com>
2021-07-27 11:14:14 +0530

    Add files via upload

---
+++ minion.js
@@ -0,0 +1,14 @@
+const translateBtn = document.querySelector("#btn-translate");
+const clearBtn = document.querySelector("#btn-clear");
+const input = document.querySelector(".in-textarea");
+const output = document.querySelector(".out-textarea");
+
+translateBtn.addEventListener('click',function onClick(){
+    const inputText = input.value;
+    const url = "https://api.funtranslations.com/translate/minion.json"+"?"+"text="+inputText;
+     fetch(url)
+    .then(response => response.json())
+    .then(json => output.innerHTML=json.contents.translated)
+    .catch(errorHandler)
+});
+const errorHandler = error => alert(error.message);
